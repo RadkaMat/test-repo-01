177d786862d8f85632ed775dbd4061aba00b916b
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2022-07-24 15:11:12 +0200

    Update uvod2.py
    oprava kódu

--- uvod2.py
+++ uvod2.py
@@ -1,3 +1,13 @@
 import streamlit as st
 st.title('Aplikace generování barev')
 st.write('Generátor náhodných barev do čtverce 5x5')
+def create_image2(color_pic):    
+    from numpy import random
+    from matplotlib import pyplot as plt
+    color_pic = random.random((5,5))
+    img = plt.imshow(color_pic)
+    img.set_cmap('inferno')
+    plt.title('inferno')
+    plt.axis('off')
+    plt.savefig("test.png")
+    obrazek4 = [[]]
