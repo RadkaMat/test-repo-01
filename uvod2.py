d4be60ec4ed21238d05706fed6e733f64bed0f4b
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2022-07-24 14:33:23 +0200

    Update uvod2.py
    přidání nadpisu do aplikace

--- uvod2.py
+++ uvod2.py
@@ -1,7 +1,3 @@
 import streamlit as st
 st.title('Aplikace generování barev')
 st.write('Generátor náhodných barev do čtverce 5x5')
-obrazek = [[1,1,1,1],[1,0,1,0],[1,1,1,1],[1,0,0,0],[1,1,0,1],]
-from matplotlib import pyplot as plt
-plt.imshow(obrazek, cmap = 'gray')
-plt.colorbar()
